75fe9d97b6b13abecfdbe33dc063abcee78d20be
Joe RN <joseph.allen.1994@hotmail.com>
2020-08-06 19:26:42 +0100

    Route test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "firebase/auth";
 import firebaseConfig from "./firebaseConfig";
 import "./App.css";
 import Child from "./Components/Child/Child";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAppAuth = firebaseApp.auth();
@@ -15,34 +16,61 @@ function App(props) {
 
   // TODO: get working on routed page
   return (
-    <div className="App">
-      <header className="App-header">
-        {user ? (
-          <p>Hello, {user.email}</p>
-        ) : (
-          <p>Sign in using email and password</p>
-        )}
-        {user ? (
-          <button onClick={signOut}>Sign out</button>
-        ) : (
-          <button
-            onClick={() =>
-              signInWithEmailAndPassword(
-                "postmantest@hotmail.com",
-                "postmantest"
-              )
-            }
-          >
-            Sign in Anonymously
-          </button>
-        )}
-        <Child
-          signInWithEmailAndPassword={signInWithEmailAndPassword}
-          signOut={signOut}
-          user={user}
-        />
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/routeTest">Route Test</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="App">
+          <header className="App-header">
+            {user ? (
+              <p>Hello, {user.email}</p>
+            ) : (
+              <p>Sign in using email and password</p>
+            )}
+            {user ? (
+              <button onClick={signOut}>Sign out</button>
+            ) : (
+              <button
+                onClick={() =>
+                  signInWithEmailAndPassword(
+                    "postmantest@hotmail.com",
+                    "postmantest"
+                  )
+                }
+              >
+                Sign in Anonymously
+              </button>
+            )}
+            <Child
+              signInWithEmailAndPassword={signInWithEmailAndPassword}
+              signOut={signOut}
+              user={user}
+            />
+            <Switch>
+              <Route path="/routeTest">
+                <p>route test</p>
+                <Child
+                  signInWithEmailAndPassword={signInWithEmailAndPassword}
+                  signOut={signOut}
+                  user={user}
+                />
+              </Route>
+              <Route path="/">
+                <p>Home test</p>
+              </Route>
+            </Switch>
+          </header>
+        </div>
+      </div>
+    </Router>
   );
 }
 
